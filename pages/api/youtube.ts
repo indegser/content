b99e0bd99542011272d69235fa8a75c7dfc1bef6
indegser <indegser@gmail.com>
2023-01-11 22:55:38 +0900

    Add unique create

--- pages/api/youtube.ts
+++ pages/api/youtube.ts
@@ -1,3 +1,4 @@
+import { isFullPage } from '@notionhq/client';
 import { notion } from '@src/sdks/notion';
 import { youtube } from '@src/sdks/youtube';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -11,6 +12,24 @@ async function fetchYoutube() {
   });
 }
 
+async function fetchFromNotion() {
+  const { results } = await notion.databases.query({
+    database_id: '22bb63060d624e398960b42c7afb7348',
+    page_size: 100,
+    sorts: [{ property: 'PublishedAt', direction: 'descending' }],
+  });
+
+  return results
+    .map((result) => {
+      if (!result || !isFullPage(result)) return;
+
+      if (result.properties.Title['type'] !== 'title') return;
+
+      return result.properties.Title.title?.[0].plain_text;
+    })
+    .filter(Boolean) as string[];
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -19,8 +38,14 @@ export default async function handler(
     data: { items = [] },
   } = await fetchYoutube();
 
+  const notionTitles = await fetchFromNotion();
+
   const promises = items.map((item) => {
     if (!item.snippet) return;
+    const { title = '' } = item.snippet!;
+
+    if (notionTitles.includes(title || '')) return;
+
     return notion.pages.create({
       parent: { database_id: '22bb63060d624e398960b42c7afb7348' },
       cover: {
