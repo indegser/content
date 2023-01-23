f0f8e2416b6d7e74823aea05b080ab8710f090c4
indegser <indegser@gmail.com>
2023-01-23 18:47:25 +0900

    Add block syncing in supabase

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -1,8 +1,10 @@
 import { isFullPage } from '@notionhq/client';
 import {
+  ListBlockChildrenParameters,
   PageObjectResponse,
   PartialPageObjectResponse,
 } from '@notionhq/client/build/src/api-endpoints';
+import { notion } from '@src/sdks/notion';
 
 const getTitleList = (
   results: Array<PageObjectResponse | PartialPageObjectResponse>
@@ -21,6 +23,25 @@ const getTitleList = (
     .filter(Boolean) as string[];
 };
 
+const getBlock = async (id: string) => {
+  const fetchNextPage = async (args: ListBlockChildrenParameters) => {
+    const result = await notion.blocks.children.list(args);
+
+    if (result.has_more) {
+      const nextResult = await fetchNextPage({
+        ...args,
+        start_cursor: result.next_cursor || undefined,
+      });
+      result.results = [...result.results, ...nextResult.results];
+    }
+
+    return result;
+  };
+
+  return fetchNextPage({ block_id: id });
+};
+
 export const notionUtils = {
   getTitleList,
+  getBlock,
 };
